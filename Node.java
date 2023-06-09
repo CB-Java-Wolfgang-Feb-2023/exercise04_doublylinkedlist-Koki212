6c12c6c9a8f51891a95251db6541babafcaceb5d
Kostadin Rizov <125398266+Koki212@users.noreply.github.com>
2023-06-09 23:52:02 +0200

    Exercise 04 - DoublyLinkedList
    Made with help from the web (programiz.com)

---
+++ Node.java
@@ -0,0 +1,36 @@
+package unit16_230603.DoublyLinkedList;
+
+public class Node {
+    int value;
+    Node next;
+    Node prev;
+
+    // Konstruktor der Node
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+}
